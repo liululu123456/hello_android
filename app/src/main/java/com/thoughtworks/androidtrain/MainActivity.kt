6298c5071fd5851ec85f529225d17a7117e2cb19
liululu123456 <626463942@qq.com>
2024-03-26 17:17:41 +0800

    feat: add btns with code

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.thoughtworks.androidtrain
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +20,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        addButtons()
+    }
+
+    private fun addButtons(){
+        val linearlayout = findViewById<LinearLayout>(R.id.main)
+        for (i in 1..20) {
+            val button = Button(this)
+
+            val buttonStringId = resources.getIdentifier("button$i", "string", packageName)
+            val buttonBackgroundColor = ContextCompat.getColor(this,R.color.gray)
+            button.text = resources.getString(buttonStringId)
+            button.setBackgroundColor(buttonBackgroundColor)
+
+            val layoutParams = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            layoutParams.topMargin = 16
+            button.layoutParams = layoutParams
+            linearlayout.addView(button)
+
+        }
+
     }
 }
