d92aff2c4e6e19efef8c566b8de552200a817bdd
liululu123456 <626463942@qq.com>
2024-03-28 15:16:06 +0800

    feat: add logs to watch life cycle

--- app/src/main/java/com/thoughtworks/androidtrain/LoginActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/LoginActivity.kt
@@ -7,6 +7,35 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_layout)
+        println("LoginActivity:onCreate-------->")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("LoginActivity:onStart-------->")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("LoginActivity:onResume-------->")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("LoginActivity:onStop-------->")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("LoginActivity:onDestroy-------->")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("LoginActivity:onRestart-------->")
     }
 
 }
