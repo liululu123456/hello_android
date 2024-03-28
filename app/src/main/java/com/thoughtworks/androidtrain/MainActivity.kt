811f2bdf3bc42a9cda91b4e7318459671ec362f7
liululu123456 <626463942@qq.com>
2024-03-28 18:01:18 +0800

    feat: jump to contact and get info

--- app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.thoughtworks.androidtrain
 
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.Manifest;
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_SELECT_CONTACT = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -55,7 +65,79 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+            if (i == 3){
+                button.setOnClickListener {
+                    requestContactsPermission()
+                }
+            }
+
         }
 
+    }
+    private fun requestContactsPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this, arrayOf<String>(Manifest.permission.READ_CONTACTS),
+                REQUEST_SELECT_CONTACT
+            )
+        } else {
+            selectContact()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_SELECT_CONTACT) {
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                selectContact()
+            } else {
+            }
+        }
+    }
+
+    private fun selectContact() {
+        val intent = Intent(Intent.ACTION_PICK).apply {
+            type = ContactsContract.Contacts.CONTENT_TYPE
+        }
+        startActivityForResult(intent, REQUEST_SELECT_CONTACT)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            val contactUri: Uri? = data?.data
+            if (contactUri != null) {
+                val cursor: Cursor? = contentResolver.query(contactUri, null, null, null, null)
+                cursor?.use {
+                    if (it.moveToFirst()) {
+                        val nameIndex = it.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME)
+                        val name = it.getString(nameIndex)
+
+                        val cursor1: Cursor? = contentResolver.query(
+                            ContactsContract.Data.CONTENT_URI,
+                            arrayOf(ContactsContract.CommonDataKinds.Phone.NUMBER)  ,
+                            null,
+                            null,
+                            null
+                        )
+
+                        cursor1?.use {
+                            while (cursor1.moveToFirst()) {
+                                val index = cursor1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+                                val phone = cursor1.getString(index)
+                                val message = "$name         $phone"
+                                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                                println(message)
+                            }}
+                    }
+                }
+            }
+        }
     }
 }
