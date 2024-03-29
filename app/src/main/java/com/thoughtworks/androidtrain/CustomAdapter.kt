76b33c88da12607438d46a58f4381a8f9fbb47fc
liululu123456 <626463942@qq.com>
2024-03-29 19:39:33 +0800

    feat: add bottom bar

--- app/src/main/java/com/thoughtworks/androidtrain/CustomAdapter.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/CustomAdapter.kt
@@ -10,30 +10,56 @@ import androidx.recyclerview.widget.RecyclerView
 import com.thoughtworks.androidtrain.model.Tweet
 
 class CustomAdapter(private val dataSet: List<Tweet>) :
-    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val VIEW_TYPE_ITEM = 0
+    private val VIEW_TYPE_BOTTOM = 1
+    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val sender: TextView
         val content: TextView
-
-
         init {
             sender = view.findViewById(R.id.sender)
             content = view.findViewById(R.id.content)
         }
     }
-
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.row_view, viewGroup, false)
-
-        return ViewHolder(view)
+    class BottomBarViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val textView: TextView
+        init {
+            textView = view.findViewById(R.id.bar)
+        }
     }
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-            viewHolder.content.text = dataSet[position].content
-            viewHolder.sender.text = dataSet[position].sender.nick
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        if (viewType == VIEW_TYPE_ITEM) {
+            val view = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.row_view, viewGroup, false)
+            return ItemViewHolder(view)
+        }
+        else {
+            val view = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.bottom_bar, viewGroup, false)
+            return BottomBarViewHolder(view)
+        }
     }
 
-    override fun getItemCount() = dataSet.size
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        if (position < dataSet.size) {
+            val itemViewHolder = viewHolder as ItemViewHolder
+            itemViewHolder.content.text = dataSet[position].content
+            itemViewHolder.sender.text = dataSet[position].sender.nick
+        }
+    }
+
+    override fun getItemViewType(position: Int):Int{
+        return if (position < dataSet.size) {
+            VIEW_TYPE_ITEM
+        } else {
+            VIEW_TYPE_BOTTOM
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size + 1
+    }
 
 }
